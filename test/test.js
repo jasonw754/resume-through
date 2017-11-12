199fb9013f7fab3077582a7228f70f4dbbdde829
Jason Williams <jason@Jasons-MBP.williams.lan>
2017-11-12 13:03:12 -0600

    Mutates the data chunk by adding a placeholder for identity.

--- test/test.js
+++ test/test.js
@@ -8,6 +8,12 @@ const resumethrough = require('../lib/resume-through');
 describe("Resume Through", function () {
     describe("basic through2 behavior", function () {
         it("wraps through2.obj", function (done) {
+            /* 
+             * Make two streams and one transformation function.
+             * Pipe the transformation onto stream 1 by just using through2.
+             * Pipe the same transformation onto stream 2 using the resume-through wrapper.
+             * Push an identical object on each stream and expect the same result from both.
+             */
             const stream1 = Readable({objectMode: true});
             stream1._read = () => {};
             
@@ -38,6 +44,29 @@ describe("Resume Through", function () {
                 }));
             
             stream1.push({x: 5});
-        })
-    })
-})
+        });
+    });
+    
+    describe("data chunk identity", function () {
+
+        it ("adds an identifier to the data chunk", function (done) {
+            /*
+             * By default, resume-through will just add a property named '__resume_through' to the data chunk.
+             * Expect the property to be there.
+             */
+            const stream = Readable({objectMode: true});
+            stream._read = () => {};
+
+            const rt = resumethrough(function (chunk, enc, cb) {
+                cb(null, chunk);
+            });
+
+            stream.pipe(rt).pipe(through2.obj(function (chunk, enc, cb) {
+                expect(chunk).to.have.property('__resume_through');
+                done();
+            }))
+
+            stream.push({});
+        });
+    });
+});
