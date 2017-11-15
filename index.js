df18921709a258dd3129e79d14aa0fea8b9bc4d6
Jason Williams <jason@Jasons-MBP.williams.lan>
2017-11-15 08:48:33 -0600

    Refactor point to introduce pipeline wrapping properly

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const through2 = require("through2");
 const uuid = require("uuid/v1");
+const miss = require("mississippi");
 
 const ResumeThrough = require("./lib/resume-through");
 
-function resumethrough(options) {
-    if (!options) {
-        return wrapStream({});
+
+function resumethrough(options, ...transforms) {
+    if (!options && transforms.length == 0) {
+        // no args passed
+        return wrap({});
     } else if (typeof options === 'function') {
-        let transformation = options;
-        options = {};
-        return wrapStream(options)(transformation);
+        // no options were passed, options is the first transform stream
+        transforms.unshift(options);
+        return wrap({})(...transforms);
     } else if (typeof options === 'object') {
-        return wrapStream(options);
+        // options were passed, and maybe transforms, we don't care here
+        return wrap(options, ...transforms);
     }
 }
 
@@ -19,22 +23,45 @@ const defaults = {
     identifier: uuid
 }
 
-function wrapStream(options) {
+function wrap(options, ...transforms) {
 
     options = Object.assign(defaults, options);
 
-    return function (name, transformation) {
-        if (typeof name === 'function' && ! transformation) {
-            transformation = name;
+    if (transforms.length == 0) {
+        // only options were passed, no transforms
+        // return a reusable function that will wrap either a pipeline or a single transform
+        return function (name, ...transforms) {
+            if (typeof name === 'function') {
+                transforms.unshift(name);
+                name = undefined;
+            }
+            if (transforms.length == 1) {
+                return wrapTransform(name, transforms[0]);
+            } else if (transforms.length > 1) {
+                return wrapPipeline(name, ...transforms);
+            }
         }
+    } else {
+        // if transforms are passed up front, passed the wrapped transform or pipeline up front
+        if (transforms.length == 1) {
+            return wrapTransform(name, transforms[0]);
+        } else if (transforms.length > 1) {
+            return wrapPipeline(name, ...transforms);
+        }
+    }
 
+    function wrapPipeline(name, ...transforms) {
+        return miss.pipeline(transforms.map(wrapTransform));
+    }
+    
+    function wrapTransform(name, transform) {
         const wrapper = function (chunk, enc, cb) {
             if (!chunk.__resume_through) {
                 chunk.__resume_through = new ResumeThrough(options, chunk);
             }
             let ticket = chunk.__resume_through;
             ticket.history.push(name);
-            transformation(chunk, enc, cb);
+            transform(chunk, enc, cb);
         }
     
         return through2.obj(wrapper);
